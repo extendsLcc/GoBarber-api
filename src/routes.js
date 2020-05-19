9f765152bcf0d1b026d75441ebed74388facf104
extendslcc <lucas.lcc@hotmail.com>
2020-05-19 16:31:22 -0300

    :star: FEAT: added association between avatar file and user.

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import auth from './app/middleware/auth';
 
 
@@ -15,8 +17,6 @@ routes.put( '/users', auth, UserController.update );
 
 routes.post( '/sessions', SessionController.store );
 
-routes.post( '/files', upload.single( 'file' ), ( req, res ) => {
-    return res.json( {ok: true} );
-} );
+routes.post( '/files', upload.single( 'file' ), FileController.store);
 
 export default routes;
