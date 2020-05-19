51b47ff68889f19e9aa09fd761070e65826b2f32
extendslcc <lucas.lcc@hotmail.com>
2020-05-19 18:04:02 -0300

    :star: FEAT: added route for creating and listing Appointments

--- src/routes.js
+++ src/routes.js
@@ -7,19 +7,25 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 
-import auth from './app/middleware/auth';
+import authMiddleware from './app/middleware/auth';
+import AppointmentController from './app/controllers/AppointmentController.js';
 
 
 const routes = new Router();
 const upload = multer( multerConfig );
 
 routes.post( '/users', UserController.store );
-routes.put( '/users', auth, UserController.update );
-
 routes.post( '/sessions', SessionController.store );
+
+routes.use( authMiddleware );
+
+routes.put( '/users', UserController.update );
 
 routes.get( '/providers', ProviderController.index );
 
-routes.post( '/files', upload.single( 'file' ), FileController.store);
+routes.post( '/appointments', AppointmentController.store );
+routes.get( '/appointments', AppointmentController.index  );
+
+routes.post( '/files', upload.single( 'file' ), FileController.store );
 
 export default routes;
