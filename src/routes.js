f1cb04a72ad2d11aa7b1ac8cf5a76ccec3cf6e52
extendslcc <lucas.lcc@hotmail.com>
2020-05-20 09:41:11 -0300

    :star: FEAT: added provider schedule listing

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middleware/auth';
 import AppointmentController from './app/controllers/AppointmentController.js';
+import ScheduleController from './app/controllers/ScheduleController.js';
 
 
 const routes = new Router();
@@ -25,6 +26,8 @@ routes.get( '/providers', ProviderController.index );
 
 routes.post( '/appointments', AppointmentController.store );
 routes.get( '/appointments', AppointmentController.index  );
+
+routes.get( '/schedule', ScheduleController.index  );
 
 routes.post( '/files', upload.single( 'file' ), FileController.store );
 
