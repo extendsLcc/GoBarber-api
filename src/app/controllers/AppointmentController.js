6e9d42084f199bc3edfed8619d595655662b82c2
extendslcc <lucas.lcc@hotmail.com>
2020-05-20 17:56:49 -0300

    :zap: IMPROVEMENT: Switch mail send to background job using redis with bee-queue.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,7 +7,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 
 class AppointmentController {
@@ -143,21 +144,8 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
-        console.log( 'reached this' );
-        await Mail.sendMail( {
-            to: `${appointment.provider.name} <${appointment.provider.email}>`,
-            subject: 'Agendamento cancelado',
-            template: 'cancellation',
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    appointment.date,
-                    `dd 'de' MM   MM', as' H:mm'h'`,
-                    { locale: pt }
-                )
-            }
-        } );
+
+        await Queue.add( CancellationMail.key, { appointment} );
 
         return res.json( appointment );
 
