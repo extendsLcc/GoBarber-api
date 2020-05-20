0e8ce5fcc7012c1be0d8c6db38f98ffb49e643dd
extendslcc <lucas.lcc@hotmail.com>
2020-05-20 08:53:00 -0300

    :star: FEAT: added pagination for Appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -10,16 +10,20 @@ class AppointmentController {
 
     async index( req, res ) {
 
+        const { page = 1 } = req.query;
+
         const appointments = await Appointment.findAll( {
             where: { user_id: req.userId, canceled_at: null },
             order: ['date'],
-            attributes: [ 'id', 'date' ],
+            attributes: ['id', 'date'],
+            limit: 20,
+            offset: ( page - 1 ) * 20,
             include: [
                 {
                     model: User,
                     as: 'provider',
                     attributes: ['id', 'name'],
-                    include:[
+                    include: [
                         {
                             model: File,
                             as: 'avatar',
