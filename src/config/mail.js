16f5f5de1a241f27aea404ad1faca28787248d9a
extendslcc <lucas.lcc@hotmail.com>
2020-05-20 16:03:56 -0300

    :heavy_plus_sign: ADD DEPENDENCY: added nodemailer setup

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '5155a3b8f8ac97',
+        pass: '3c9c9d34cba578'
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>'
+    }
+};
