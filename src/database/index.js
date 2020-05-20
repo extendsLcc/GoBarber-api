10645bdd8c06a88a651d5015e4ef888cd280db2e
extendslcc <lucas.lcc@hotmail.com>
2020-05-20 10:03:33 -0300

    :heavy_plus_sign: ADD DEPENDENCY: add MongoDB  connection and mongoose dependency

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -14,6 +15,7 @@ class Database {
     constructor() {
 
         this.init();
+        this.mongo();
 
     }
 
@@ -24,6 +26,19 @@ class Database {
         models
             .map( model => model.init( this.connection ) )
             .map( model => model.associate && model.associate( this.connection.models ) );
+
+    }
+
+    mongo() {
+
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/gobarber',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: true,
+                useUnifiedTopology: true
+            }
+        );
 
     }
 
